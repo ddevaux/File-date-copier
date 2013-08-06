56a736d13964aa0a5fd2984ab3c0bfb2bd65620f
Dorian Devaux <ddevaux@intuitiv.fr>
2013-08-06 18:26:23 +0200

    Initial code commit

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-File-date-copier
-================
+# File-date-copier #
+
+Saves and restores file dates across computers. I needed this after a long FTP transfer completely ignored the last accessed/modified dates.
+
+This tool copy and restore `atime` and `mtime`. Creation time is ignored (on Unix it is not saved anyway)
+
+# Usage #
+
+	usage: fileDateCopier.py [-h] [-d DATA] {R,W} [path]
+
+	File date copier
+
+	positional arguments:
+	  {R,W}                 Mode Read/Write
+	  path
+
+	optional arguments:
+	  -h, --help            show this help message and exit
+	  -d DATA, --data DATA  File to write/read from
+
+First, grab the dates :
+
+*The default path is the current directory*
+
+	python fileDateCopier.py R
+
+Copy the `dates.dat` to where you want the dates restored
+
+Then, simple run
+
+	python fileDateCopier.py W
+
+
+
+# Requirements #
+
+Python 2.7 with no additional modules (standard library). Uses `argparse` hence the 2.7
+
---
+++ fileDateCopier.py
@@ -0,0 +1,55 @@
+# coding: utf8
+import os
+from collections import OrderedDict
+import argparse
+import cPickle
+ 
+parser = argparse.ArgumentParser(description = "File date copier")
+parser.add_argument("mode", choices=['R','W'], help="Mode Read/Write")
+parser.add_argument("path", default=".", nargs="?")
+parser.add_argument("-d", "--data", default="dates.dat", help="File to write/read from")
+args = parser.parse_args()
+ 
+if args.mode == "R":
+ 
+	pathdates = OrderedDict()
+	
+	total_files, total_dirs = 0, 0
+ 
+	def save_path(path):
+		pathdates[path] = (os.path.getatime(path), os.path.getmtime(path))
+ 
+	for dirpath, dirnames, filenames in os.walk(unicode(args.path)):
+		total_files += len(filenames)
+		total_dirs += len(dirnames)
+		for d in dirnames:
+			save_path(os.path.join(dirpath, d))
+		
+		for f in filenames:
+			save_path(os.path.join(dirpath, f))
+			
+			
+	f = open(args.data, "wb")
+	cPickle.dump(pathdates, f)
+	f.close()
+	
+	print "Found %d files, %d directories" % (total_files, total_dirs)
+	
+elif args.mode == "W":
+	
+	f = open(args.data, "rb")
+	pathdates = cPickle.load(f)
+	f.close()
+	
+	os.chdir(args.path)
+	
+	total_updated = 0
+	
+	for path, dates in pathdates.iteritems():
+		if os.path.exists(path):
+			os.utime(path, dates)
+			total_updated += 1
+		
+	print "Updated %d files/directories out of %d entries" % (total_updated, len(pathdates))
+	
+	
